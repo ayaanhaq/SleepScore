060ac8ac1941344b5aba0f4da8aedcdf8ce83fc2
ayaanhaq <ayaanhaq1106@gmail.com>
2025-08-01 11:26:33 +0530

    Add files via upload

---
+++ SleepScore/SleepScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepScoreApp.swift
+//  SleepScore
+//
+//  Created by Ayaan Haq on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SleepScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
